84e18889823802cda231692238eac968d65bcca8
Drew Chase <dcmanproductions@gmail.com>
2021-03-16 12:14:51 -0400

    Adding Profile Section

---
+++ Flexx.Web.Service/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace com.drewchaseproject.net.Flexx.Web.Service.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Profile");
+        }
+        [Route("/Account/Profile")]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
