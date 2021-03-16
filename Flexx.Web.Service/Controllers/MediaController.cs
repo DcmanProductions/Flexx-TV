47a69c498f048b7e160fa95419deb1fb786dcbe8
Drew Chase <dcmanproductions@gmail.com>
2021-03-16 12:07:30 -0400

    Changed Dashboard to Media

--- Flexx.Web.Service/Controllers/MediaController.cs
+++ Flexx.Web.Service/Controllers/MediaController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace com.drewchaseproject.net.Flexx.Web.Service.Controllers
 {
-    public class DashboardController : Controller
+    public class MediaController : Controller
     {
         public IActionResult Index()
         {
@@ -18,14 +18,20 @@ namespace com.drewchaseproject.net.Flexx.Web.Service.Controllers
         {
             return View();
         }
-        [Route("/Movies/{movieName}")]
+        [Route("/Media/Movies/{movieName}")]
         public IActionResult MediaView(string movieName)
         {
             MovieModel movie = LibraryListModel.Singleton.GetByName("movies").Movies.GetByName(movieName);
             return View(movie);
         }
-        [Route("/Movies/{movieName}/Trailer")]
+        [Route("/Media/Movies/{movieName}/Trailer")]
         public IActionResult Trailer(string movieName)
+        {
+            MovieModel movie = LibraryListModel.Singleton.GetByName("movies").Movies.GetByName(movieName);
+            return View(movie);
+        }
+        [Route("/Media/Movies/{movieName}/Watch")]
+        public IActionResult Watch(string movieName)
         {
             MovieModel movie = LibraryListModel.Singleton.GetByName("movies").Movies.GetByName(movieName);
             return View(movie);
