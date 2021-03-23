46d712a13f49df88f7ecb041c92b13eb7b16632c
Drew Chase <dcmanproductions@gmail.com>
2021-03-22 23:41:37 -0400

    Working on Series

---
+++ Flexx.Media/Libraries/Series/SeriesModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.drewchaseproject.net.Flexx.Media.Libraries.Series
+{
+    public class SeriesModel : MediaModel
+    {
+    }
+}
