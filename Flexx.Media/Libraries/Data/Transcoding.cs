5643cd0aeb912fbf389ad1336a05d49b8a16dbf5
Drew Chase <dcmanproductions@gmail.com>
2021-04-12 15:39:55 -0400

    Figuring out Transcoding

--- Flexx.Media/Libraries/Data/Transcoding.cs
+++ Flexx.Media/Libraries/Data/Transcoding.cs
@@ -7,16 +7,24 @@ namespace Flexx.Media.Libraries.Data
 {
     public class Transcoding
     {
-        private static readonly Transcoding _singleton;
+        private static Transcoding _singleton;
         public static Transcoding Singleton => _singleton ?? new Transcoding();
         private Transcoding()
         {
+            _singleton = this;
             if (Directory.GetFiles(Values.FFMPEGDirectory, "*ffmpeg*", SearchOption.TopDirectoryOnly).Length == 0)
             {
                 FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official, Values.FFMPEGDirectory).Wait();
             }
 
             FFmpeg.SetExecutablesPath(Values.FFMPEGDirectory);
+        }
+
+        public FileStream GetTranscodedStream(string file)
+        {
+            //var stream = FFmpeg.GetMediaInfo(file).Result.Streams.ToArray()[0];
+            //FFmpeg.GetMediaInfo(file).Result.VideoStreams.ToArray()[0].
+            return null;//new FileStream(..SetCodec(VideoCodec.mpeg4).SetBitrate(4000).SetSize(VideoSize.Pal).Split(new System.TimeSpan(0, 32, 55), new System.TimeSpan(0, 45, 55))., FileMode.Open, FileAccess.Read);
         }
     }
 }
