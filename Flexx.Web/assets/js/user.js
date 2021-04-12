ede6b3eddd6064987983c50e5f8b75c811f14cc3
Drew Chase <dcmanproductions@gmail.com>
2021-04-12 15:45:29 -0400

    Code Cleanup

--- Flexx.Web/assets/js/user.js
+++ Flexx.Web/assets/js/user.js
@@ -1,4 +1,4 @@
-var server = "https://play.drewchaseproject.com:2112";
+var server = "http://play.drewchaseproject.com:2112";
 // var server = "https://localhost:2112";
 var userName = "";
 var isLoggedIn = false;
