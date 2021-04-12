a6e23fbda173b2549d44b2bb13593ea6463ebb10
Drew Chase <dcmanproductions@gmail.com>
2021-04-12 15:36:49 -0400

    Updated Git Ignore
    Removed Kestral Web Server in favor of REST_API

--- Flexx.Web.Service/Areas/Identity/Data/FlexxUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Flexx.Web.Service.Areas.Identity.Data
-{
-    // Add profile data for application users by adding properties to the FlexxUser class
-    public class FlexxUser : IdentityUser
-    {
-    }
-}
