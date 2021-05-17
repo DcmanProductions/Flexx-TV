bcf4d74cb48914234274b228690ec967801b1489
Drew Chase <dcmanproductions@gmail.com>
2021-05-17 12:53:45 -0400

    Moved clients to individual git repos

--- Flexx.Web/assets/js/builder.js
+++
@@ -1,8 +0,0 @@
-document.title = `${document.title} - FlexxTV`
-let uri = document.baseURI.replaceAll(document.baseURI.split('/')[document.baseURI.split('/').length - 1], "")+window.location.hash;
-window.history.pushState("", "", uri);
-Array.from(document.getElementsByClassName("builder")).forEach(item => {
-    let id = item.id;
-    let builder = item.dataset.builder;
-    $(`#${id}`).load(`/Pages/builder/${builder}.html`);
-});
--- Flexx.Web/assets/js/navigation.js
+++
@@ -1,8 +0,0 @@
-﻿function changeActiveNavItem(id) {
-    var items = document.getElementsByClassName('navItem');
-    Array.from(items).forEach(i => {
-        i.classList.remove('active');
-    })
-    if (id != -1)
-        items[id].classList.add('active');
-}
